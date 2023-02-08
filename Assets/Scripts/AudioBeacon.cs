aeadc1a7e3b5c1e4ac62f8f6c15b0b423c02081a
Thelarion <46968231+Thelarion@users.noreply.github.com>
2023-02-08 17:41:34 +0100

    Update Audio Beacon

--- Assets/Scripts/AudioBeacon.cs
+++ Assets/Scripts/AudioBeacon.cs
@@ -8,42 +8,34 @@ public class AudioBeacon : MonoBehaviour
     public GameObject targetBeacon;
     public AK.Wwise.Event BeaconLR;
     public AK.Wwise.Event BeaconLock;
-    private float _pan;
-    public bool StartBeacon = false;
+    private float _panning;
     private bool _beaconActive = false;
     private bool _beaconLock_hasPlayed = false;
 
     private void Start()
     {
-        AngleDir();
+        // AngleDir();
     }
 
     private void Update()
     {
-        ToggleBeaconIO();
-
         if (_beaconActive)
         {
             AngleDir();
         }
     }
 
-    private void ToggleBeaconIO()
+    public void ToggleBeaconOn()
     {
-        if (StartBeacon)
-        {
-            if (!_beaconActive)
-            {
-                BeaconLR.Post(transform.gameObject);
-                _beaconActive = true;
-            }
-            else
-            {
-                BeaconLR.Stop(transform.gameObject);
-                _beaconActive = false;
-            }
-            StartBeacon = false;
-        }
+        BeaconLR.Post(transform.gameObject);
+        _beaconActive = true;
+    }
+
+    public void ToggleBeaconOff()
+    {
+        BeaconLR.Stop(transform.gameObject);
+        _beaconActive = false;
+        _beaconLock_hasPlayed = false;
     }
 
     private bool myApproximation(float a, float b, float tolerance)
@@ -55,7 +47,21 @@ public class AudioBeacon : MonoBehaviour
         var directionToEnemy = targetBeacon.transform.position - transform.position;
         var projectionOnRight = Vector3.Dot(directionToEnemy, transform.right);
 
-        if (myApproximation(projectionOnRight, 0f, 0.5f))
+        float directionFace = Vector3.Dot(transform.forward, (targetBeacon.transform.position - transform.position).normalized);
+
+
+        // if (myApproximation(projectionOnRight, 0f, 0.5f))
+        // {
+        //     Debug.Log("FRONT");
+        //     AkSoundEngine.SetRTPCValue("beacon_lr_volume", 0);
+
+        //     if (!_beaconLock_hasPlayed && _beaconActive)
+        //     {
+        //         BeaconLock.Post(gameObject);
+        //         _beaconLock_hasPlayed = true;
+        //     }
+        // }
+        if (directionFace > 0.99f)
         {
             Debug.Log("FRONT");
             AkSoundEngine.SetRTPCValue("beacon_lr_volume", 0);
@@ -68,37 +74,34 @@ public class AudioBeacon : MonoBehaviour
         }
         else if (projectionOnRight < 0)
         {
-            // Debug.Log("LEFT");
-
-            // Reset the front locking sound
-            // Also make sure beacon plays from the beginning when looking away
-            if (_beaconLock_hasPlayed)
-            {
-                BeaconLR.Stop(transform.gameObject);
-                BeaconLR.Post(transform.gameObject);
-                _beaconLock_hasPlayed = false;
-            }
-
-            _pan = -1f;
-            AkSoundEngine.SetRTPCValue("left_right", _pan);
-            AkSoundEngine.SetRTPCValue("beacon_lr_volume", 1);
+            Debug.Log("LEFT");
+            ResetLRAndLock();
+            SetRTPCAndVolume(-1f);
         }
         else if (projectionOnRight > 0)
         {
-            // Debug.Log("RIGHT");
+            Debug.Log("RIGHT");
+            ResetLRAndLock();
+            SetRTPCAndVolume(1f);
+        }
+    }
 
-            // Reset the front locking sound
-            // Also make sure beacon plays from the beginning when looking away
-            if (_beaconLock_hasPlayed)
-            {
-                BeaconLR.Stop(transform.gameObject);
-                BeaconLR.Post(transform.gameObject);
-                _beaconLock_hasPlayed = false;
-            }
-            _pan = 1f;
-            AkSoundEngine.SetRTPCValue("left_right", _pan);
-            AkSoundEngine.SetRTPCValue("beacon_lr_volume", 1);
+    private void SetRTPCAndVolume(float LR)
+    {
+        _panning = LR;
+        AkSoundEngine.SetRTPCValue("left_right", _panning);
+        AkSoundEngine.SetRTPCValue("beacon_lr_volume", 1);
+    }
 
+    private void ResetLRAndLock()
+    {
+        // Reset the front locking sound
+        // Also make sure beacon plays from the beginning when looking away
+        if (_beaconLock_hasPlayed)
+        {
+            BeaconLR.Stop(transform.gameObject);
+            BeaconLR.Post(transform.gameObject);
+            _beaconLock_hasPlayed = false;
         }
     }
 }
